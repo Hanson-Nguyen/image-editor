2a4fea7dc5c12b5fd299ca3e8b86163e55f72be0
Hanson Nguyen <hansonn00@gmail.com>
2021-06-04 23:58:59 -0400

    added open image button

--- image_edit_gui.py
+++ image_edit_gui.py
@@ -1,6 +1,9 @@
 import tkinter as tk
-from tkinter import filedialog, Text
+from PIL import Image
+from tkinter import filedialog, Text, Button
 import os
+
+path = "cat.jpg"
 
 gui = tk.Tk()
 gui.resizable(True, True)
@@ -14,6 +17,12 @@ frame = tk.Frame(gui, bg = "white")
 frame.place(relwidth = 1, relheight = 0.8, relx = 0, rely = 0)
 frame.configure(background = 'teal')
 
+def openImage():
+    img = Image.open(path)
+    img.show()
+
+buttonOpenImage = Button(gui, text = "Open Image", command = openImage)
+buttonOpenImage.pack()
 
 gui.mainloop()
 
