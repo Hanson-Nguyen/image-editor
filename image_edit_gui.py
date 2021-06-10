fd3a085ced2c2d13859888cd8afff43496ed5077
Hanson Nguyen <hansonn00@gmail.com>
2021-06-10 02:49:29 -0400

    update select image functionality
    selecting an image now displays it onto gui. also added exception handler.

--- image_edit_gui.py
+++ image_edit_gui.py
@@ -4,7 +4,7 @@ from PIL import Image
 from PIL import ImageTk
 from tkinter import filedialog, Text, Button, PhotoImage, Canvas
 import tkinter.filedialog
-import tkFileDialog
+import tkinter.messagebox
 
 gui = tk.Tk()
 gui.resizable(True, True)
@@ -19,33 +19,40 @@ frame.configure(background = 'teal')
 canvas = Canvas(gui, width = 750, height = 750, highlightthickness = 0, bg = 'teal')
 canvas.grid()
 
-def openImage(self):
-    img = Image.open("test1.gif")
-
-    #checking size of image to resize image and keep ratio
-    width, height = img.size
+global myfile
+def selectImage(self):
 
     wratio = 1
     hratio = 1
 
-    if (height > width):
-        wratio = width/height 
-    if (width > height):
-        hratio = height/width
+    while True:
+        try: 
+            myfile = filedialog.askopenfilename()
+            print(myfile)
 
-    img2 = img.resize((round(wratio * 750), round(hratio * 750)), Image.ANTIALIAS)
-    resized = ImageTk.PhotoImage(img2)
-    self.img = resized
-    canvas.create_image(0, 0, anchor = "nw", image = resized) 
+            img = Image.open(myfile)
 
-content = 'apple'
-file_path = 'banana'
+            #checking size of image to resize image and keep ratio
+            width, height = img.size
 
-def selectImage(self):
-    myfile = filedialog.askopenfilename()
+            if (height > width):
+                wratio = width/height 
+            if (width > height):
+                hratio = height/width
 
-buttonOpenImage = Button(gui, text = "Open Image", command = lambda:openImage(gui))
-buttonOpenImage.grid(row = 5, column = 3)
+            img2 = img.resize((round(wratio * 750), round(hratio * 750)), Image.ANTIALIAS)
+            resized = ImageTk.PhotoImage(img2)
+            self.img = resized
+            canvas.create_image(0, 0, anchor = "nw", image = resized) 
+
+        except Exception as e:
+            tkinter.messagebox.showinfo('Error','This is an invalid image file. Please select an image with extensions: .png, .jpg, .gif')
+
+        else:
+            break
+
+#buttonOpenImage = Button(gui, text = "Open Image", command = lambda:openImage(gui))
+#buttonOpenImage.grid(row = 5, column = 3)
 
 buttonChooseImage = Button(gui, text = "Select Image", command = lambda:selectImage(gui))
 buttonChooseImage.grid(row = 6, column = 3)
